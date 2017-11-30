bb682a011f3e21eb378f66103ea0e42cc3756842
Grant Kennedy <grantkennedy@gmail.com>
2017-11-30 00:10:32 +0000

    attempting to optimise...

--- image.py
+++ image.py
@@ -1,8 +1,11 @@
 import numpy as np
-
+import los_funcs
 
 class Dens(object):
-    '''Define some density functions.'''
+    '''Define some density functions.
+    
+    Should in general peak at or near 1, to aid finding integration box.
+    '''
 
     def __init__(self,model='gauss_3d',gaussian_scale_height=0.05):
         '''Get an object to do density.
@@ -40,7 +43,7 @@ class Dens(object):
     # Gaussian torus and parameters
     gauss_3d_params = ['$r_0$','$\sigma_r$','$\sigma_h$']
     def gauss_3d(self, r, az, el, p):
-        '''Gaussian torus.'''
+#        '''Gaussian torus.'''
         return np.exp( -( 0.5*(r-p[0])/p[1] )**2 ) * \
                     np.exp( -0.5*(r*np.sin(el)/p[2])**2 )
 
@@ -124,12 +127,17 @@ class Image(object):
         '''
 
         self.image_size = image_size
-        self.rmax_arcsec = rmax_arcsec
-        self.arcsec_pix = arcsec_pix
-        self.rmax = int(rmax_arcsec / arcsec_pix)
         self.model = model
         self.emit_model = emit_model
         self.dens_model = dens_model
+        self.arcsec_pix = arcsec_pix
+
+        # set fixed some things needed to make images
+        self.nx, self.ny = self.image_size
+        self.nx2 = self.nx // 2
+        self.ny2 = self.ny // 2
+        if rmax_arcsec is not None:
+            self.set_rmax(rmax_arcsec)
 
         # set the image model
         self.select(model)
@@ -138,6 +146,7 @@ class Image(object):
         d = Dens(model=dens_model,**dens_args)
         self.dens = d.dens
         self.dens_params = d.params
+        self.n_dens_params = len(self.dens_params)
 
         # paste the params together
         self.params = self.image_params + self.dens_params
@@ -168,22 +177,44 @@ class Image(object):
         self.image = models[model]['fit_func']
         self.image_full = models[model]['full_func']
         self.image_params = models[model]['params']
+        self.n_image_params = len(self.image_params)
+
+
+    def compute_rmax(self, p, tol=1e-5):
+        '''Figure out model extent to make image generation quicker.
+        
+        Work inwards, stopping when density becomes non-zero.
+        '''
+    
+        for r in self.arcsec_pix*np.arange(np.max([self.nx2,self.ny2]),1,-1):
+            for az in np.arange(0,360,30):
+                for el in np.arange(-90,90,15):
+                    if self.dens(r,az,el,p[self.n_image_params:]) > tol:
+                        self.set_rmax(r)
+                        print('found r_max: {} ({} pix)'.format(r,self.rmax))
+                        return None
+
+
+    def set_rmax(self,rmax_arcsec):
+        '''Set rmax and associated things.'''
+
+        self.rmax_arcsec = rmax_arcsec
+        self.rmax = int(rmax_arcsec / self.arcsec_pix)
+        self.crop_size = self.rmax * 2
+        a = np.arange(self.crop_size) - (self.crop_size-1)/2.
+        self.zarray, self.yarray = np.meshgrid(a, a)
 
 
     los_image_full_params = ['$x_0$','$y_0$','$\Omega$','$f$','$i$','$F$']
-    def los_image_full(self,p):
+    def los_image_full(self,p,save_bounds=False):
         '''Return an image of a disk.
 
         Heavily 'borrows' from zodipic.
         '''
         
         x0, y0, pos, anom, inc, tot = p[:6]
-
-        crop_size = self.rmax * 2
-        nx, ny = self.image_size
-        image = np.zeros((ny, nx))
-        nx2 = nx // 2
-        ny2 = ny // 2
+        image = np.zeros((self.ny, self.nx))
+        yarray = self.yarray - y0
 
         # some geometry, angles are -ve because the
         # rotation matrices go clockwise instead of a.c.w
@@ -191,47 +222,46 @@ class Image(object):
         s0 = np.sin(np.deg2rad(-pos))
         c1 = np.cos(np.deg2rad(inc))
         s1 = np.sin(np.deg2rad(inc))
-        c2 = np.cos(np.deg2rad(-anom)+np.pi/2)
-        s2 = np.sin(np.deg2rad(-anom)+np.pi/2)
+        c2 = np.cos(np.deg2rad(-anom+np.pi/2))
+        s2 = np.sin(np.deg2rad(-anom+np.pi/2))
         c0c1c2 = c0 * c1 * c2
         c0c1s2 = c0 * c1 * s2
         c0s1 = c0 * s1
         s0s2 = s0 * s2
+        s0c1 = s0 * c1
         s0c2 = s0 * c2
-
-        # z and y arrays
-        a = np.arange(crop_size) - (crop_size-1)/2.
-        zarray, yarray = np.meshgrid(a, a)
-        yarray -= y0
+        c0c1c2_s0s2 = c0c1c2 - s0s2
+        c0c1s2_s0c2 = c0c1s2 + s0c2
 
         # x-independent parts of the coordinate transformation
-        trans1 = -(s0*c1*c2 + c0*s2)*yarray + s1*c2*zarray
-        trans2 = -(s0*c1*s2 - c0*c2)*yarray + s1*s2*zarray
-        trans3 = s0*s1*yarray + c1*zarray
+        trans1 = -(s0c1*c2 + c0*s2)*yarray + s1*c2*self.zarray
+        trans2 = (-s0c1*s2 + c0*c2)*yarray + s1*s2*self.zarray
+        trans3 = s0*s1*yarray + c1*self.zarray
 
         # go through slice by slice and get the flux along an x column
-        for i in np.arange(crop_size):
+        for i in np.arange(self.crop_size):
 
             x = i + 0.5 - self.rmax - x0
 
             # x,y,z locations in original model coords
-            x3 = (c0c1c2 - s0s2)*x + trans1
-            y3 = (c0c1s2 + s0c2)*x + trans2
+            x3 = c0c1c2_s0s2*x + trans1
+            y3 = c0c1s2_s0c2*x + trans2
             z3 = -c0s1*x + trans3
 
             # get the spherical polars
-            rxy2 = x3*x3 + y3*y3
+            rxy2 = x3**2 + y3**2
             rxy = np.sqrt(rxy2)
-            r = np.sqrt(rxy2 + z3*z3) * self.arcsec_pix
+            r = np.sqrt(rxy2 + z3**2) * self.arcsec_pix
+            # these two lines are as expensive as dens below
             az = np.arctan2(y3,x3)
             el = np.arctan2(z3,rxy)
 
-            # the density in this y,z later
+            # the density in this y,z layer
             layer = self.emit(r) * self.dens(r,az,el,p[6:])
 
             # put this in the image
-            image[ny2-self.rmax:ny2+self.rmax, nx2-self.rmax+i] = \
-                                np.sum(layer,axis=1)
+            image[self.ny2-self.rmax:self.ny2+self.rmax,
+                  self.nx2-self.rmax+i] = np.sum(layer,axis=1)
             
         return tot * image / np.sum(image)
 
