e551b2f0911411da42d01fd16c654205fc8dbc7f
Grant Kennedy <grantkennedy@gmail.com>
2017-12-05 22:27:29 +0000

    travis

--- tests/test_dens.py
+++ tests/test_dens.py
@@ -15,20 +15,20 @@ def test_dens_types():
     for t in types:
         d.select(t)
         npar = len(d.params)
-        d.dens(1,0,0,np.arange(npar)+1)
+        d.dens(1,0,0,np.arange(npar)+1.0)
 
 def test_dens_gauss_2d():
     r = np.arange(50)
-    r0 = 25
-    w = 4
+    r0 = 25.
+    w = 4.
     de = np.exp(-0.5*((r-r0)/w)**2)
     d = alma.image.Dens(model='gauss_2d')
     assert(np.all(np.equal(de,d.dens(r,0,0,[r0,w]))))
 
 def test_other_dens_function():
     r = np.arange(50)
-    r0 = 25
-    w = 4
+    r0 = 25.
+    w = 4.
     f = lambda r,r0,w: np.exp(-0.5*((r-r0)/w)**3)
     de = f(r,r0,w)
     d = alma.image.Dens(model='gauss_2d')
