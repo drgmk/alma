121151f9867b14fe4d0f62a6c7e58904291f8ed7
Grant Kennedy <grantkennedy@gmail.com>
2018-04-07 16:46:54 +0100

    fix rv shift

--- alma/cube.py
+++ alma/cube.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 def v_rad(x,r,inc,mstar):
-    '''Return radial velocity in km/s.
+    '''Return radial velocity in an inclined disk in km/s.
     
     Parameters
     ----------
     x : float or ndarray
-        Sky distance to pixel along disk major axis.
+        Sky distance to pixel along disk major axis, in m.
     r : float or ndarray
         Distance to pixel in m.
     inc : float
@@ -20,8 +20,10 @@ def v_rad(x,r,inc,mstar):
 
 
 def stack_vel(c, inclination=None, pa=None, mstar=None, x0=0.0, y0=0.0,
-              arcsec_pix=None, distance=None, vel_pix=None, mask=None):
+              arcsec_pix=None, distance=None, vel_pix=None):
     '''Stack sky/velocity cube for a given inclination.
+    
+    .. todo: implement mask keyword
     
     Parameters
     ----------
@@ -34,9 +36,9 @@ def stack_vel(c, inclination=None, pa=None, mstar=None, x0=0.0, y0=0.0,
     mstar : float
         Stellar mass in Solar masses.
     x0 : float
-        Pixel offset of center from image center.
+        Offset of disk center from image center, in arcsec.
     y0 : float
-        Pixel offset of center from image center.
+        Offset of disk center from image center, in arcsec.
     arcsec_pix : float
         Pixel scale of (square) image pixels.
     distance : float
@@ -47,32 +49,32 @@ def stack_vel(c, inclination=None, pa=None, mstar=None, x0=0.0, y0=0.0,
         Mask used to restrict pixels where velocites are stacked.
     '''
 
-    # distances
+    # distances in pixels
     sh = c.shape
-    x = np.arange(sh[1]) - sh[1]/2 + 0.5 - x0
-    y = np.arange(sh[0]) - sh[0]/2 + 0.5 - y0
+    x = np.arange(sh[1]) - sh[1]/2 + 0.5 - x0 / arcsec_pix
+    y = np.arange(sh[0]) - sh[0]/2 + 0.5 - y0 / arcsec_pix
     x, y = np.meshgrid(x, y)
     r_sky = np.sqrt(x*x + y*y)
 
-    # distance along major axis
+    # along major axis
     t = np.array([[np.cos(np.deg2rad(pa)),-np.sin(np.deg2rad(pa))],
                   [np.sin(np.deg2rad(pa)), np.cos(np.deg2rad(pa))]])
     XY = np.vstack( (x.reshape(-1), y.reshape(-1)) )
     xy = np.matmul(t.T, XY)
     x, y = xy.reshape( (2,) + x.shape )
 
-    # distance to pixel in plane of orbit
+    # to pixel in plane of orbit
     r = np.sqrt( r_sky*r_sky + (x*np.tan(np.deg2rad(inclination)))**2 )
 
     # velocities in each pixel
     sc = arcsec_pix * distance * 1.496e11
     vr = v_rad(y * sc, r * sc, inclination, mstar)
 
-    # shift cube by rolling each sky pixel,
-    # do this by shifting the x's in each layer of y
+    # shift cube by rolling each sky pixel
     c_shift = np.zeros(sh)
     shift = np.round(vr / vel_pix).astype(int)
     for i in range(sh[0]):
-        c_shift[i] = np.roll(c[i], tuple(shift[i]), 1)
+        for j in range(sh[1]):
+            c_shift[i,j,:] = np.roll(c[i,j,:], shift[i,j])
 
     return shift, c_shift
