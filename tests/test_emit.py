ec583b723debb94e87e6a6189b59d1a25ddb1b81
Grant Kennedy <grantkennedy@gmail.com>
2018-05-30 21:51:34 +0100

    add some blackbody tests

--- tests/test_emit.py
+++ tests/test_emit.py
@@ -10,6 +10,15 @@ def test_list_models():
     assert(isinstance(e.select(list_models=True),list))
 
 def test_blackbody():
+    e = alma.image.Emit(model='blackbody')
+    assert(np.allclose(e.emit(1,[1,100],1000),
+                       e.emit(1,[1,100],2000)*4))
+    assert(np.allclose(e.emit(1,[1,100],1000),
+                       e.emit(1,[1,200],1000)/2))
+    assert(np.allclose(e.emit(1,[1,100],1000),
+                       e.emit(0.5,[1,100],1000)/np.sqrt(2)))
+
+def test_rj_tail():
     r = 1 + np.arange(10)
     t = 1 / r**0.5
     e = alma.image.Emit(model='rj_tail')
