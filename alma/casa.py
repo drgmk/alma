245a8c96271ac6e2a9e0fa5d63d6e9329689345d
drgmk <grantkennedy@gmail.com>
2023-06-28 20:26:40 +0100

    updates for modular casa fitting

--- alma/casa.py
+++ alma/casa.py
@@ -1,6 +1,17 @@
 import os
 import numpy as np
 
+# test what we are using and import appropriately
+try:
+    __casashell_state__
+    print('Assuming packaged CASA')
+except NameError:
+    print('Assuming modular CASA')
+    import casatools.ms
+    import casatools.table
+    ms = casatools.ms()
+    tb = casatools.table()
+
 '''CASA functions related to fitting models to ALMA data.'''
 
 def residual(ms, vis_model, tb, datacolumn='CORRECTED_DATA',
@@ -276,3 +287,145 @@ def export_ms(msfile, tb, ms, outfile='uv.npy', timescan=False):
         np.save(outputfilename, [u, v, Re, Im, w, time, scan])
     else:
         np.save(outputfilename, [u, v, Re, Im, w])
+
+
+def get_ms_vis(msfilename, xcor=True, acor=False):
+    """Direct copy of Luca Matra's export.
+    https://github.com/dlmatra/miao
+    """
+
+    cc = 2.9979e10  # cm/s
+
+    # Use CASA table tools to get columns of UVW, DATA, WEIGHT, etc.
+    tb.open(msfilename)
+    data    = tb.getcol("DATA")
+    uvw     = tb.getcol("UVW")
+    weight  = tb.getcol("WEIGHT")
+    ant1    = tb.getcol("ANTENNA1")
+    ant2    = tb.getcol("ANTENNA2")
+    flags   = tb.getcol("FLAG")
+    spwid   = tb.getcol("DATA_DESC_ID")
+    time    = tb.getcol("TIME")
+    # scan    = tb.getcol("SCAN_NUMBER")
+    tb.close()
+
+    if np.any(flags):
+        print(f"{msfilename}: some of the data is FLAGGED")
+
+    print("Found data with "+str(data.shape[-1])+" uv points")
+    if data.shape[-1] == 0:
+        return [], [], [], [], []
+
+    ms.open(msfilename)
+    spw_info = ms.getspectralwindowinfo()
+    nchan = spw_info[list(spw_info.keys())[0]]['NumChan']
+    npol  = spw_info[list(spw_info.keys())[0]]['NumCorr']
+    ms.close()
+    print("with "+str(nchan)+" channels per SPW and "+str(npol)+" polarizations,")
+
+    # Use CASA table tools to get frequencies, which are needed to
+    # calculate u-v points from baseline lengths
+    tb.open(msfilename+"/SPECTRAL_WINDOW")
+    freqs = tb.getcol("CHAN_FREQ")
+    rfreq = tb.getcol("REF_FREQUENCY")
+    wave = 2.9979e8/np.average(rfreq)*1e3
+    tb.close()
+
+    print(str(freqs.shape[1])+" SPWs and Channel 0 frequency of 1st SPW of "+str(rfreq[0]/1e9)+" GHz")
+    print("corresponding to "+str(2.9979e8/rfreq[0]*1e3)+" mm")
+    print("Average wavelength is "+str(wave)+" mm")
+
+    if np.max(uvw[0:2, :]) == 0:
+        logging.info('No non-zero baselines, returning')
+        return [], [], [], [], []
+
+    print("Dataset has baselines between "+str(np.min(np.sqrt(uvw[0, :]**2.0+uvw[1, :]**2.0))) +
+                 " and "+str(np.max(np.sqrt(uvw[0, :]**2.0+uvw[1, :]**2.0)))+" m")
+
+    # Initialize u and v arrays (coordinates in Fourier space)
+    uu = np.zeros((freqs.shape[0], uvw[0, :].size))
+    vv = np.zeros((freqs.shape[0], uvw[0, :].size))
+
+    # Fill u and v arrays appropriately from data values.
+    for i in np.arange(freqs.shape[0]):
+        for j in np.arange(uvw.shape[1]):
+            uu[i, j] = uvw[0, j]*freqs[i, spwid[j]]/(cc/100.0)
+            vv[i, j] = uvw[1, j]*freqs[i, spwid[j]]/(cc/100.0)
+
+    # Extract real and imaginary part of the visibilities at all u-v coordinates,
+    # for both polarization states (XX and YY), extract weights which correspond
+    # to 1/(uncertainty)^2
+    Re_xx = data[0,:,:].real
+    Im_xx = data[0,:,:].imag
+    weight_xx = weight[0,:]
+    if npol>=2:
+        Re_yy = data[1,:,:].real
+        Im_yy = data[1,:,:].imag
+        weight_yy = weight[1,:]
+
+        # Since we don't care about polarization, combine polarization states
+        # (average them together) and fix the weights accordingly. Also if any
+        # of the two polarization states is flagged, flag the outcome of the
+        # combination.
+        flags = np.logical_or(flags[0,:,:],flags[1,:,:])
+        Re = np.where((weight_xx + weight_yy) != 0, (Re_xx*weight_xx + Re_yy*weight_yy) / (weight_xx + weight_yy), 0.)
+        Im = np.where((weight_xx + weight_yy) != 0, (Im_xx*weight_xx + Im_yy*weight_yy) / (weight_xx + weight_yy), 0.)
+        wgts = (weight_xx + weight_yy)
+    else:
+        Re=Re_xx
+        Im=Im_xx
+        wgts=weight_xx
+        flags=flags[0,:,:]
+
+    # Find which of the data represents cross-correlation between two
+    # antennas as opposed to auto-correlation of a single antenna.
+    # We don't care about the latter so we don't want it.
+    xc = np.where(ant1 != ant2)[0]
+    ac = np.where(ant1 == ant2)[0]
+    if xcor:
+        if acor:
+            xc = np.logical_or(xc, ac)
+    elif acor:
+        xc = ac
+
+    # Select data
+    time = time[xc]
+    # scan = scan[xc]
+    data_real = Re[:, xc]
+    data_imag = Im[:, xc]
+    flags = flags[:, xc]
+    # data_wgts = wgts[xc]
+    data_uu = uu[:, xc]
+    data_vv = vv[:, xc]
+    data_wgts = np.reshape(np.repeat(wgts[xc], uu.shape[0]), data_uu.shape)
+
+    # Select only data that is NOT flagged, this step has the unexpected
+    # effect of flattening the arrays to 1d
+    data_real = data_real[np.logical_not(flags)]
+    data_imag = data_imag[np.logical_not(flags)]
+    # flags_ = flags[np.logical_not(flags)]
+    data_wgts = data_wgts[np.logical_not(flags)]
+    data_uu = data_uu[np.logical_not(flags)]
+    data_vv = data_vv[np.logical_not(flags)]
+    time = time[np.logical_not(flags[0])]
+    # scan = scan[np.logical_not(flags[0])]
+
+    time /= (24*60*60)  # to MJD
+    vis = data_real + 1j*data_imag
+
+    # sort into time order
+    srt = np.argsort(time)
+    data_uu = data_uu[srt]
+    data_vv = data_vv[srt]
+    vis = vis[srt]
+    data_wgts = data_wgts[srt]
+    time = time[srt]
+
+    # re-weighting as suggested by Loomis+
+    wgt_mean = np.mean(data_wgts)
+    data_std = np.std(vis)
+    rew = (1/data_std**2)/wgt_mean
+    print(f're-weighting value (1dof): {rew}')
+    data_wgts *= rew
+
+    return data_uu, data_vv, vis, data_wgts, wave
