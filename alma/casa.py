4554ee68c4cd4f4bbaccac33d4ca1b7142192e7e
Grant Kennedy <g.kennedy@warwick.ac.uk>
2023-07-04 16:12:19 +0100

    updates

--- alma/casa.py
+++ alma/casa.py
@@ -14,7 +14,7 @@ except NameError:
 
 '''CASA functions related to fitting models to ALMA data.'''
 
-def residual(ms, vis_model, tb, datacolumn='CORRECTED_DATA',
+def residual_standalone(ms, vis_model, tb, datacolumn='CORRECTED_DATA',
              ms_new='residual.ms', remove_new=True,
              model_ms=None, remove_model=True):
     '''Create a new ms with the model subtracted.
@@ -58,6 +58,58 @@ def residual(ms, vis_model, tb, datacolumn='CORRECTED_DATA',
     # reshape for subtraction to [nchan,nrow]
     vis_mod = np.load(vis_model)
     print('Opened model file with shape {}'.format(vis_mod.shape))
+    vis_mod = vis_mod.reshape(nchan, nrow)
+
+    # subtract model from each polarisation
+    sub = data - vis_mod
+
+    # put the data back in the table and close
+    tb.putcol(datacolumn, sub)
+    tb.close()
+
+
+def residual(ms, vis_mod, datacolumn='DATA',
+             ms_new='residual.ms', remove_new=True):
+    """Create a new ms with the model subtracted.
+
+    Parameters
+    ----------
+    ms : str
+        ms file to subtract from.
+    vis_model : str
+        File with model visibilities to subtract.
+    tb : function
+        casa tb function.
+    datacolumn : str
+        Column to subtract model from.
+    ms_new : str, optional
+        ms file to create and put residual data in.
+    remove_new : bool, optional
+        Remove ms_new before proceeding.
+    """
+
+    # copy ms to a new ms that we will modify
+    if os.path.exists(ms_new):
+        if remove_new:
+            os.system('rm -r '+ms_new)
+
+    if not os.path.exists(ms_new):
+        os.system('cp -r '+ms+' '+ms_new)
+    else:
+        raise FileExistsError('file {} exists'.format(ms_new))
+
+    # open the ms and get the data, this has shape [2,nchan,nrow], where
+    # the 2 is two polarisations
+    tb.open(ms_new, nomodify=False)
+    data = tb.getcol(datacolumn)
+    nchan = data.shape[1]
+    nrow = data.shape[2]
+    print('Opened ms file with {} channels, {} rows'.format(nchan,nrow))
+
+    # open the model visibilities, which is a vector nchan x nrow long
+    # of complex visibilities, and each row is tiled nchan times.
+    # reshape for subtraction to [nchan,nrow]
+    print('Model with shape {}'.format(vis_mod.shape))
     vis_mod = vis_mod.reshape(nchan, nrow)
 
     # subtract model from each polarisation
@@ -194,8 +246,7 @@ def export_ms(msfile, tb, ms, outfile='uv.npy', timescan=False):
         print("Note: some of the data is FLAGGED")
     print("Found data with "+str(data.shape[-1])+" uv points")
 
-
-    #Use CASA ms tools to get the channel/spw info
+    # use CASA ms tools to get the channel/spw info
     ms.open(msfilename)
     spw_info = ms.getspectralwindowinfo()
     nchan = spw_info["0"]["NumChan"]
@@ -215,11 +266,11 @@ def export_ms(msfile, tb, ms, outfile='uv.npy', timescan=False):
 
     print("Datasets has baselines between "+str(np.min(np.sqrt(uvw[0,:]**2.0+uvw[1,:]**2.0)))+" and "+str(np.max(np.sqrt(uvw[0,:]**2.0+uvw[1,:]**2.0)))+" m")
 
-    #Initialize u and v arrays (coordinates in Fourier space)
+    # Initialize u and v arrays (coordinates in Fourier space)
     uu=np.zeros((freqs.shape[0],uvw[0,:].size))
     vv=np.zeros((freqs.shape[0],uvw[0,:].size))
 
-    #Fill u and v arrays appropriately from data values.
+    # Fill u and v arrays appropriately from data values.
     for i in np.arange(freqs.shape[0]):
         for j in np.arange(uvw.shape[1]):
             uu[i,j]=uvw[0,j]*freqs[i,spwid[j]]/(cc/100.0)
@@ -398,6 +449,7 @@ def get_ms_vis(msfilename, xcor=True, acor=False):
     data_uu = uu[:, xc]
     data_vv = vv[:, xc]
     data_wgts = np.reshape(np.repeat(wgts[xc], uu.shape[0]), data_uu.shape)
+    time = np.tile(time, data_uu.shape[0]).reshape(data_uu.shape[0], -1)
 
     # Select only data that is NOT flagged, this step has the unexpected
     # effect of flattening the arrays to 1d
@@ -407,7 +459,7 @@ def get_ms_vis(msfilename, xcor=True, acor=False):
     data_wgts = data_wgts[np.logical_not(flags)]
     data_uu = data_uu[np.logical_not(flags)]
     data_vv = data_vv[np.logical_not(flags)]
-    time = time[np.logical_not(flags[0])]
+    time = time[np.logical_not(flags)]
     # scan = scan[np.logical_not(flags[0])]
 
     time /= (24*60*60)  # to MJD
