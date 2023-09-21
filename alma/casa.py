232f467d0b19f35e64ccd297749b56e54017b871
drgmk <grantkennedy@gmail.com>
2023-09-21 12:02:21 +0100

    explicit shape for weights

--- alma/casa.py
+++ alma/casa.py
@@ -461,6 +461,8 @@ def get_ms_vis(msfilename, xcor=True, acor=False, reweight=True, sort=False):
         Re_yy = data[1,:,:].real
         Im_yy = data[1,:,:].imag
         weight_yy = weight[1,:]
+        weight_xx = np.tile(weight_xx, nchan).reshape(nchan, -1)
+        weight_yy = np.tile(weight_yy, nchan).reshape(nchan, -1)
 
         # Since we don't care about polarization, combine polarization states
         # (average them together) and fix the weights accordingly. Also if any
@@ -494,10 +496,10 @@ def get_ms_vis(msfilename, xcor=True, acor=False, reweight=True, sort=False):
     data_real = Re[:, xc]
     data_imag = Im[:, xc]
     flags = flags[:, xc]
-    # data_wgts = wgts[xc]
+    data_wgts = wgts[:, xc]
     data_uu = uu[:, xc]
     data_vv = vv[:, xc]
-    data_wgts = np.reshape(np.repeat(wgts[xc], uu.shape[0]), data_uu.shape)
+    # data_wgts = np.reshape(np.repeat(wgts[xc], uu.shape[0]), data_uu.shape)
     time = np.tile(time, data_uu.shape[0]).reshape(data_uu.shape[0], -1)
     spws = np.tile(spws, data_uu.shape[0]).reshape(data_uu.shape[0], -1)
 
